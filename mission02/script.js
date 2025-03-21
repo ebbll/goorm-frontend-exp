f368b88b1f3852e431f28e03b88584f8821aa135
eunlee <eklee2057@naver.com>
2025-03-21 12:50:30 +0900

    todo app completed

--- mission02/script.js
+++ mission02/script.js
@@ -5,15 +5,16 @@ let todos = [];
 
 createBtn.addEventListener("click", createNewTodo);
 
+document.addEventListener("DOMContentLoaded", loadFromLocalStorage);
+
 function createNewTodo() {
   const item = {
     id: new Date().getTime(),
-    text: "EXP 미션 완수하기",
+    text: "",
     complete: false,
   };
 
-  //   todos = [...todos, item];
-  todos = unshift(item);
+  todos.unshift(item);
 
   const { itemElement, inputElement, editBtnElement, removeBtnElement } =
     createTodoElement(item);
@@ -21,15 +22,20 @@ function createNewTodo() {
   list.prepend(itemElement);
 
   inputElement.removeAttribute("disabled");
+
   inputElement.focus();
+
+  saveToLocalStorage();
 }
 
 function createTodoElement(item) {
   const itemElement = document.createElement("div");
-  itemElement.classList.add("item");
+  itemElement.classList.add("todo-item");
 
   const checkboxElement = document.createElement("input");
   checkboxElement.type = "checkbox";
+  checkboxElement.classList.add("todo-check");
+  checkboxElement.checked = item.complete;
 
   if (item.complete) {
     itemElement.classList.add("complete");
@@ -39,17 +45,51 @@ function createTodoElement(item) {
   inputElement.type = "text";
   inputElement.value = item.text;
   inputElement.setAttribute("disabled", "");
+  inputElement.classList.add("todo-text");
 
   const actionsElement = document.createElement("div");
   actionsElement.classList.add("actions");
 
   const editBtnElement = document.createElement("button");
-  editBtnElement.classList.add("material-icons");
-  editBtnElement.innerText = "Edit";
+  editBtnElement.classList.add("edit-btn", "action-btn");
+  editBtnElement.innerText = "수정";
 
   const removeBtnElement = document.createElement("button");
-  removeBtnElement.classList.add("material-icons", "remove-btn");
-  removeBtnElement.innerText = "remove_circles";
+  removeBtnElement.classList.add("remove-btn", "action-btn");
+  removeBtnElement.innerText = "삭제";
+
+  checkboxElement.addEventListener("change", () => {
+    item.complete = checkboxElement.checked;
+
+    if (item.complete) {
+      itemElement.classList.add("complete");
+    } else {
+      itemElement.classList.remove("complete");
+    }
+    saveToLocalStorage();
+  });
+
+  inputElement.addEventListener("blur", () => {
+    inputElement.setAttribute("disabled", "");
+    saveToLocalStorage();
+  });
+
+  inputElement.addEventListener("input", () => {
+    item.text = inputElement.value;
+    saveToLocalStorage();
+  });
+
+  editBtnElement.addEventListener("click", () => {
+    inputElement.removeAttribute("disabled");
+    inputElement.focus();
+  });
+
+  removeBtnElement.addEventListener("click", () => {
+    todos = todos.filter((t) => t.id !== item.id);
+
+    itemElement.remove();
+    saveToLocalStorage();
+  });
 
   actionsElement.append(editBtnElement);
   actionsElement.append(removeBtnElement);
@@ -59,4 +99,24 @@ function createTodoElement(item) {
   itemElement.append(actionsElement);
 
   return { itemElement, inputElement, editBtnElement, removeBtnElement };
+}
+
+function saveToLocalStorage() {
+  const data = JSON.stringify(todos);
+
+  window.localStorage.setItem("my_todos", data);
+}
+
+function loadFromLocalStorage() {
+  const data = window.localStorage.getItem("my_todos");
+
+  if (data) {
+    todos = JSON.parse(data);
+
+    todos.forEach((item) => {
+      const { itemElement } = createTodoElement(item);
+
+      list.append(itemElement);
+    });
+  }
 }
