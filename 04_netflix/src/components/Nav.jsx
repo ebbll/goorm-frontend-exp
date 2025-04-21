619863803ad2ebcddeff7e9b4827095a6d329b5f
eunlee <eklee2057@naver.com>
2025-04-21 12:22:41 +0900

    nav 컴포넌트에 배경색 및 클래스 이름 추가

--- 04_netflix/src/components/Nav.jsx
+++ 04_netflix/src/components/Nav.jsx
@@ -19,7 +19,7 @@ export default function Nav() {
   }, []);
 
   return (
-    <nav>
+    <nav className={`nav ${show ? "nav_black" : ""}`}>
       <img
         alt="Netflix Logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
