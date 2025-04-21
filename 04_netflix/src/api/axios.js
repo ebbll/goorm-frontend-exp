649a008683fb28ca58b5b53512a64905f8cbbc35
eunlee <eklee2057@naver.com>
2025-04-21 13:33:56 +0900

    axios 인스턴스의 baseURL을 올바른 API URL로 수정

--- 04_netflix/src/api/axios.js
+++ 04_netflix/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://www.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "baf54fa07f07a2bf7c23d509262f0afb",
     language: "ko-KR",
