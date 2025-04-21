4a7bddfcf35ea77c998f4fb355e6b025fc7bac2a
eunlee <eklee2057@naver.com>
2025-04-21 09:15:54 +0900

    넷플릭스 프로젝트 생성

---
+++ 04_netflix/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://www.themoviedb.org/3",
+  params: {
+    api_key: "baf54fa07f07a2bf7c23d509262f0afb",
+    language: "ko-KR",
+  },
+});
+
+export default instance;
