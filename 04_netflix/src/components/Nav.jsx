15195e056a0286abb9ad4a2ef276e77e0b10e717
eunlee <eklee2057@naver.com>
2025-05-01 18:26:14 +0900

    Nav 컴포넌트의 레이아웃 및 스타일 수정, 메뉴 항목 추가

--- 04_netflix/src/components/Nav.jsx
+++ 04_netflix/src/components/Nav.jsx
@@ -20,17 +20,42 @@ export default function Nav() {
 
   return (
     <nav className={`nav ${show ? "nav_black" : ""}`}>
-      <img
-        alt="Netflix Logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
-        className="nav_logo"
-        onClick={() => window.location.reload()}
-      />
-      <img
-        alt="User logged"
-        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
-        className="nav_avatar"
-      />
+      <div className="nav_left">
+        <img
+          alt="Netflix Logo"
+          src="/src/assets/netflix-logo.png"
+          className="nav_logo"
+          onClick={() => window.location.reload()}
+        />
+        <ul className="nav_menu">
+          <li className="nav_menu_item">홈</li>
+          <li className="nav_menu_item">시리즈</li>
+          <li className="nav_menu_item">영화</li>
+          <li className="nav_menu_item">게임</li>
+          <li className="nav_menu_item">NEW! 요즘 대세 콘텐츠</li>
+          <li className="nav_menu_item">내가 찜한 리스트</li>
+          <li className="nav_menu_item">언어별로 찾아보기</li>
+        </ul>
+      </div>
+      <div className="nav_right">
+        <ul className="nav_avatar_menu">
+          <li>
+            <img
+              alt="User logged"
+              src="/src/assets/netflix-avatar.jpg"
+              className="nav_avatar"
+            />
+            <ul className="depth_1">
+              <li className="nav_avatar_menu_item">프로필 관리</li>
+              <li className="nav_avatar_menu_item">프로필 이전</li>
+              <li className="nav_avatar_menu_item">계정</li>
+              <li className="nav_avatar_menu_item">고객 센터</li>
+              <hr />
+              <li className="nav_avatar_menu_item">넷플릭스에서 로그아웃</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
