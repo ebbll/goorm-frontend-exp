0b66acef99d6b84abf2db3f4014c59d0a2f3f7c6
eunlee <eklee2057@naver.com>
2025-05-01 18:27:13 +0900

    Footer 컴포넌트 추가: 스타일 및 정보 섹션 구현

---
+++ 04_netflix/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-grid">
+        <div className="footer-grid-item">화면 해설</div>
+        <div className="footer-grid-item">고객 센터</div>
+        <div className="footer-grid-item">기프트카드</div>
+        <div className="footer-grid-item">미디어 센터</div>
+        <div className="footer-grid-item">투자 정보(IR)</div>
+        <div className="footer-grid-item">입사 정보</div>
+        <div className="footer-grid-item">이용 약관</div>
+        <div className="footer-grid-item">개인정보</div>
+        <div className="footer-grid-item">법적 고지</div>
+        <div className="footer-grid-item">쿠키 설정</div>
+        <div className="footer-grid-item">회사 정보</div>
+        <div className="footer-grid-item">문의하기</div>
+      </div>
+      <div className="footer-info">
+        <div>
+          넷플릭스서비시스코리아 유한회사 통신판매업신고번호:
+          제2018-서울종로-0426호 전화번호: 00-308-321-0161 (수신자 부담)
+        </div>
+        <div>대표: 레지널드 숀 톰프슨</div>
+        <div>이메일 주소: korea@netflix.com</div>
+        <div>
+          주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층
+          우편번호 03161
+        </div>
+        <div>사업자등록번호: 165-87-00119</div>
+        <div>클라우드 호스팅: Amazon Web Services Inc.</div>
+        <div>공정거래위원회 웹사이트</div>
+      </div>
+    </footer>
+  );
+}
