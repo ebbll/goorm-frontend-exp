baf88b7d54b1dc1133d5d5cd8a27993c7f17ac07
eunlee <eklee2057@naver.com>
2025-05-02 10:36:15 +0900

    추가: SearchPage 컴포넌트 및 스타일 추가, 영화 검색 기능 구현

--- 04_netflix/src/pages/SearchPage/SearchPage.jsx
+++ 04_netflix/src/pages/SearchPage/SearchPage.jsx
@@ -1,3 +1,88 @@
+import "./SearchPage.css";
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDebounce } from "../../hooks/useDebounce";
+import MovieModal from "../../components/MovieModal/MovieModal";
+
 export default function SearchPage() {
-  return <div></div>;
+  const nav = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  useEffect(() => {
+    if (debouncedSearchTerm === "" || debouncedSearchTerm === null) {
+      nav("/");
+      return;
+    }
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
+    }
+  }, [debouncedSearchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  onClick={() => handleClick(movie)}
+                  //   onClick={() => nav(`/${movie.id}`)}
+                  className="movie_column-poster"
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie_poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+        {modalOpen && (
+          <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+        )}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results_text">
+          <p>
+            {`입력하신 검색어 '${searchTerm}'(와)과 일치하는 결과가 없습니다.`}
+          </p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 }
